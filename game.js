886a5580f9b5f839cd59da0dc8b1c258678495a6
Emily Amspoker <eamspoker21@kentdenver.org>
2020-10-22 23:15:08 -0600

    added infinite runner aspect

--- game.js
+++ game.js
@@ -21,6 +21,7 @@
 
   var score = 0;
   var scoreText;
+  var speed = 1;
 
   function preload ()
   {
@@ -35,37 +36,23 @@
   function create ()
   {
     // add sky
-    this.add.image(400,300,'sky');
+    this.add.image(400,300,'sky').setScrollFactor(0);
 
-    // create group of platforms
-    platforms = this.physics.add.staticGroup();
+    // create ground using tileSprite
+    ground = this.physics.add.staticGroup();
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    // add physics, set gravity to zero
+    ground.create(0, config.height, 'ground').setScale(4).refreshBody();
 
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
 
     // create player
 
-    player = this.physics.add.sprite(100, 450, 'dude');
+    player = this.physics.add.sprite(0, 450, 'dude');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
+
     // creating the animation of the sprite
-
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
-      frameRate: 10,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'turn',
-      frames: [ { key: 'dude', frame: 4 } ],
-      frameRate: 20
-    });
 
     this.anims.create({
       key: 'right',
@@ -74,6 +61,10 @@
       repeat: -1
     });
 
+    player.anims.play('right', true);
+
+
+
 
   // setting the gravity of the player
 
@@ -81,19 +72,20 @@
 
   // add collider between the player and any platforms
 
-  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(player, ground);
+
 
   // create stars
 
   stars = this.physics.add.group({
     key: 'star',
-    repeat: 11,
-    setXY: {x: 12, y: 0, stepX: 70}
+    repeat: 5,
+    setXY: {x: 0, y: config.height - 80, stepX: 160}
   });
 
  // add star collider with platforms
 
- this.physics.add.collider(stars, platforms);
+ this.physics.add.collider(stars, ground);
 
  // let the user collect stars
 
@@ -105,42 +97,56 @@
 
   // create text for the score
   scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+  scoreText.setScrollFactor(0)
 
-  // create bomb group and colliders
 
-  bombs = this.physics.add.group();
-
-  this.physics.add.collider(bombs, platforms);
-
-  this.physics.add.collider(player, bombs, hitBomb, null, this);
 
 }
 
 function update ()
 {
-  cursors = this.input.keyboard.createCursorKeys();
+    var cam = this.cameras.main;
+    cam.scrollX += speed;
+    this.physics.world.bounds.left = cam.worldView.left + 30;
+    this.physics.world.bounds.right = cam.worldView.right;
 
-  if(cursors.left.isDown){
-    player.setVelocityX(-300);
-    player.anims.play('left', true);
-  } else if (cursors.right.isDown){
-    player.setVelocityX(300);
-    player.anims.play('right', true);
-  } else {
-    player.setVelocityX(0);
-    player.anims.play('turn');
-  }
+    Phaser.Actions.Call(stars.getChildren(), function(sprite) {
+      if(sprite.x + 30 < this.physics.world.bounds.left){
+          sprite.destroy();
+      }
+    }, this);
 
-  if(cursors.space.isDown && player.body.touching.down){
-    player.setVelocityY(-500);
-  }
+    Phaser.Actions.Call(ground.getChildren(), function(sprite) {
+      if(sprite.x + 30 < this.physics.world.bounds.left){
+          sprite.destroy();
+      }
+    }, this);
+
+    if(ground.getFirst(true).x <= this.physics.world.bounds.left){
+      ground.create(this.physics.world.bounds.left, config.height, 'ground').setScale(4).refreshBody();
+    }
+
+    if(stars.getChildren().length <= 4){
+      stars.create(this.physics.world.bounds.right - 10, config.height-85, 'star');
+
+    }
+
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    if(cursors.space.isDown && player.body.touching.down){
+      player.setVelocityY(-600);
+      player.setVelocityX(0);
+    } else if(player.body.touching.down){
+      player.setVelocityX(0);
+    }
 }
 
 
 function collectStar(player, star){
 
   // disable the star's physics body and make it invisible
-  star.disableBody(true, true);
+  star.destroy();
 
   score += 10;
 
@@ -148,30 +154,4 @@ function collectStar(player, star){
 
   // refills stars if they're all gone
 
-  if(stars.countActive(true) === 0){
-    stars.children.iterate(function(child){
-      child.enableBody(true, child.x, 0, true, true);
-    });
-
-  // creates a random bomb
-
-  var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-  }
-
-
-}
-
-function hitBomb(player, bomb){
-  // ends the game and turns the player red
-
-  this.physics.pause();
-
-  player.setTint(0xff0000);
-  player.anims.play('turn');
-  gameOver = true;
 }
