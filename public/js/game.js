6a5ba7a7c9aeb2392f8c806255a6f8921ef2877f
Emily Amspoker <eamspoker21@kentdenver.org>
2020-10-24 14:03:11 -0600

    replace game.js with update

--- public/js/game.js
+++ public/js/game.js
@@ -18,42 +18,51 @@
   };
 
   var game = new Phaser.Game(config);
-
   var score = 0;
   var scoreText;
   var speed = 1;
   var star_dist = 5;
   var old_score = 0;
-  var obstacle_prob = 10;
   var health = 4;
-  var platform_num = 1;
+  var old_time = 0;
+
 
   function preload ()
   {
+    this.load.plugin('rexclockplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexclockplugin.min.js', true);
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
     this.load.spritesheet('dude', 'assets/dude.png',
           { frameWidth: 32, frameHeight: 48});
+
   }
 
   function create ()
   {
+    clock = this.plugins.get('rexclockplugin').add(this, config);
+    clock.start();
     // add sky
-    this.add.image(400,300,'sky').setScrollFactor(0);
+    sky = this.add.image(400, 300,'sky').setScrollFactor(0);
+
+
+
 
     // create ground
     ground = this.physics.add.staticGroup();
-    ground.create(0, config.height, 'ground').setScale(4).refreshBody();
+    ground_1 = ground.create(0, config.height, 'ground').setScale(2).refreshBody();
+
 
     // create platforms
     platforms = this.physics.add.staticGroup();
-
+    platform_1 = platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(700, 220, 'ground');
 
     // create player
 
-    player = this.physics.add.sprite(0, 450, 'dude');
+    player = this.physics.add.sprite(0, 0, 'dude');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
@@ -88,7 +97,7 @@
   stars = this.physics.add.group({
     key: 'star',
     repeat: 5,
-    setXY: {x: 0, y: config.height - 80, stepX: 160}
+    setXY: {x: 0, y: 0, stepX: 160}
   });
 
  // add star collider with platforms
@@ -104,7 +113,6 @@
 
  obstacles = this.physics.add.group();
 
- obstacles.create(this.physics.world.bounds.right, 16, 'bomb');
 
  this.physics.add.collider(obstacles, ground);
  this.physics.add.collider(obstacles, platforms);
@@ -118,7 +126,7 @@
   scoreText.setScrollFactor(0)
 
   // create text for health
-  healthText = this.add.text(config.width/2, 16, 'Health: 4', { fontSize: '32px', fill: '#000' });
+  healthText = this.add.text(scoreText.width*1.5 + scoreText.x, 16, 'Health: 4', { fontSize: '32px', fill: '#000' });
   healthText.setScrollFactor(0)
 
 
@@ -150,6 +158,12 @@ function update ()
       }
     }, this);
 
+    Phaser.Actions.Call(platforms.getChildren(), function(sprite) {
+      if(sprite.x + 30 + sprite.width < this.physics.world.bounds.left){
+          sprite.destroy();
+      }
+    }, this);
+
     if(ground.getFirst(true).x <= this.physics.world.bounds.left){
       ground.create(this.physics.world.bounds.left, config.height, 'ground').setScale(4).refreshBody();
     }
@@ -158,8 +172,26 @@ function update ()
     // need to change with music implementation
 
     if(stars.getChildren().length <= star_dist - 1){
-      stars.create(this.physics.world.bounds.right - 10, config.height-85, 'star');
+      var star_x = Phaser.Math.Between(this.physics.world.bounds.left, this.physics.world.bounds.right);
+      stars.create(this.physics.world.bounds.right - 10, 0, 'star');
+
+
     }
+
+    // creates new plaforms every 3 seconds
+    // need to change with music implementation
+
+    if(clock.now - old_time > 6000/speed){
+      var platform_resize =  Phaser.Math.Between(1, 20);
+      var platform_y = Phaser.Math.Between(100, config.height-ground_1.height-platform_1.height-player.height);
+      new_platform = platforms.create(this.physics.world.bounds.right, platform_y + 71, 'ground').setScale(platform_resize/20, 1);
+      new_platform.x = this.physics.world.bounds.right + new_platform.width;
+      new_platform.body.updateFromGameObject();
+      old_time = clock.now;
+    }
+
+
+
 
 
     cursors = this.input.keyboard.createCursorKeys();
@@ -181,16 +213,32 @@ function update ()
         star_dist -= 1;
       }
       old_score = score;
-      obstacles.create(this.physics.world.bounds.right, 16, 'bomb');
+
+      var obstacle_y = Phaser.Math.Between(100, config.height-ground_1.height-player.height-81);
+      new_obstacle = obstacles.create(this.physics.world.bounds.right, obstacle_y, 'bomb');
+      var obstacle_platform_resize =  Phaser.Math.Between(1, 20);
+      new_obs_platform = platforms.create(this.physics.world.bounds.right, obstacle_y + 71, 'ground');
+      new_obs_platform.setScale(obstacle_platform_resize/20, 1);
+      new_obs_platform.x = this.physics.world.bounds.right + new_obs_platform.width;
+      new_obs_platform.body.updateFromGameObject();
+      new_obstacle.x = this.physics.world.bounds.right + new_obs_platform.width;
+
     }
+
+
 
     // no health, game over
     if(health <= 0){
       gameOver = true;
     }
 
-}
 
+
+
+
+
+
+}
 
 
 
@@ -210,8 +258,9 @@ function collectStar(player, star){
   star.destroy();
 
   score += 10;
-
   scoreText.setText('Score: ' + score);
+
+
 
 
 }
